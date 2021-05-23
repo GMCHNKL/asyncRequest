14f6af33492be6dbdfd79de7c32a4c5d2afc1fc9
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2021-05-23 18:25:01 +0530

    file saveed on 2021-05-23 18:25:00.755917

--- executable/gitHandle.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from datetime import datetime
-gitadd = subprocess.run(["git","add","."])
-print(gitadd)
-gitcommit = subprocess.run(["git", "commit",'-m','file save'+str(datetime.now())])
-print(gitcommit)
-gitpush = subprocess.run(["git","push"])
-print(gitpush)
-
