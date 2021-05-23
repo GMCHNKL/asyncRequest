df9027ff83b99a2c7866af1541257e26ff6af1a0
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2021-05-23 18:17:38 +0530

    file saveed on 2021-05-23 18:17:37.931879

--- gitHandle.py
+++ gitHandle.py
@@ -9,7 +9,8 @@
 
 import os
 from datetime import datetime
-os.system('git remote add git@github.com:GMCHNKL/asyncRequest.git')
+os.system('git remote add origin git@github.com:GMCHNKL/asyncRequest.git')
+os.system('git status')
 os.system('git add .')
 os.system('git commit -m "file saveed on '+str(datetime.now())+'"')
 os.system('git push origin main')
