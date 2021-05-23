6f62b42aa9cd26c2a25abe651e09d8c9e5ce71bf
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2021-05-23 18:10:53 +0530

    file saveed on 2021-05-23 18:10:52.802003

--- gitHandle.py
+++ gitHandle.py
@@ -1,9 +1,16 @@
-import subprocess
+# import subprocess
+# from datetime import datetime
+# gitadd = subprocess.run(["git","add","."])
+# print(gitadd)
+# gitcommit = subprocess.run(["git", "commit",'-m','file save'+str(datetime.now())])
+# print(gitcommit)
+# gitpush = subprocess.run(["git","push"])
+# print(gitpush)
+
+import os
 from datetime import datetime
-gitadd = subprocess.run(["git","add","."])
-print(gitadd)
-gitcommit = subprocess.run(["git", "commit",'-m','file save'+str(datetime.now())])
-print(gitcommit)
-gitpush = subprocess.run(["git","push"])
-print(gitpush)
+os.system('git remote git@github.com:GMCHNKL/asyncRequest.git')
+os.system('git add .')
+os.system('git commit -m "file saveed on '+str(datetime.now())+'"')
+os.system('git push')
 
